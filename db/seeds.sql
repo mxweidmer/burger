8fa8f00b2606f9ae43c3275ed2dab1f2b84a002d
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-04-05 21:05:54 -0700

    added seed data for db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Sliders');
+INSERT INTO burgers (burger_name) VALUES ('Plain old burger');
