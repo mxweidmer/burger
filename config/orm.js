12958c23d6305e5fe1ae1467c305f041fdf06679
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-04-05 21:50:17 -0700

    created update one function in orm.js (kinda pulled from catsapp for creating querystring)

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,21 @@
 
 var connection = require("./connection.js");
 
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
 var orm = {
     selectAll: function (table, cb) {
         connection.query("SELECT * FROM ??;",
@@ -12,11 +27,28 @@ var orm = {
         )
     },
     insertOne: function (table, cols, vals, cb) {
-        connection.query("INSERT INTO ?? (?) VALUES (?)",
-            [table, cols, vals],
+        connection.query("INSERT INTO ?? (??) VALUES (??);",
+            [table, cols.toString(), vals.toString()],
             function (err, result) {
                 if (err) throw err;
                 cb(result);
             });
     },
+    updateOne: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
 }
+
+module.exports = orm;
